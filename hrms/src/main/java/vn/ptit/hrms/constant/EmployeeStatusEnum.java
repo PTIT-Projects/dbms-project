497ddaedd53e97cb5f9f2e8bc6303d35785d7f02
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2025-03-19 23:20:39 +0700

    Edit enum

--- hrms/src/main/java/vn/ptit/hrms/constant/EmployeeStatusEnum.java
+++ hrms/src/main/java/vn/ptit/hrms/constant/EmployeeStatusEnum.java
@@ -1,5 +1,16 @@
 package vn.ptit.hrms.constant;
 
+
 public enum EmployeeStatusEnum {
-    Terminated, Expired, Active
+    TERMINATED("Terminated"), EXPIRED("Expired"), ACTIVE("Active");
+
+    EmployeeStatusEnum(String value) {
+        this.value = value;
+    }
+
+    private final String value;
+
+    public String getValue() {
+        return value;
+    }
 }
--- hrms/src/main/java/vn/ptit/hrms/constant/GenderEnum.java
+++ hrms/src/main/java/vn/ptit/hrms/constant/GenderEnum.java
@@ -1,5 +1,14 @@
 package vn.ptit.hrms.constant;
 
 public enum GenderEnum {
-    Other, Female, Male
+    OTHER("Other"), FEMALE("Female"), MALE("Male");
+    private final String value;
+
+    GenderEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
