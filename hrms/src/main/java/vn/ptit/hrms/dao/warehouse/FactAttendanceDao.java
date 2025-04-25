6fe0deaa5e6de4f02e3b80e44a9f9033cc2455b7
Cong Tuan Duong Vu <DuongVCT.B22KH024@stu.ptit.edu.vn>
2025-04-25 17:07:32 +0700

    update

--- hrms/src/main/java/vn/ptit/hrms/dao/warehouse/FactAttendanceDao.java
+++ hrms/src/main/java/vn/ptit/hrms/dao/warehouse/FactAttendanceDao.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
